752df107517a9df175174c1542f5d08b0edf89cd
ThomasTaylorJr <jjtaylor1925@gmail.com>
2021-04-24 00:16:18 -0400

    fix: modify rss menu item

--- publishconf.py
+++ publishconf.py
@@ -8,3 +8,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 RELATIVE_URLS = False
+
+# Feeds
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+FEED_ALL_RSS = "feeds/all.rss.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
